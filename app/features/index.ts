414f30c5099a2acd0bd40f0f6fda855656ddce87
martin-bucinskas <7595909+martin-bucinskas@users.noreply.github.com>
2021-05-27 17:07:03 +0100

    ipv-paas-cf-fix: Re-arranging the package.json deps.
    - Moved some dependencies around.
    
    Moved some dependencies around as they were causing issues to them not being found once starting it up on PaaS.
    
    - Using webpack.dev.js
    
    Using the dev webpack config, as the prod doesn't seem to load -> might be fixed after the dependency shuffle.
    
    - Added docker-compose.yml
    
    Added a docker compose file to help with local orchestration.

--- app/features/index.ts
+++ app/features/index.ts
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 import "./common/accessibiltyStatement";
 import "./common/appIcons";
 import "./common/cookies";
