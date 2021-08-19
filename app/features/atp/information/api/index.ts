7697ad58e9ac8070da6efa5726a15a7384fa9399
martin-bucinskas <7595909+martin-bucinskas@users.noreply.github.com>
2021-08-19 14:59:00 +0100

    BAU: Fixing broken ATPs.
    - Some refactoring and fixing the linting which wasn't run before.
    - Removing functions within functions and replacing them with arrow functions.
    - Using sessionData in req.session.

--- app/features/atp/information/api/index.ts
+++ app/features/atp/information/api/index.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 import { getBasicInfoAPI } from "./config";
-const jwt = require("jsonwebtoken");
 
 export async function postBasicInfoJSON(data: any): Promise<any> {
   const result = await axios.post(getBasicInfoAPI(), data);
-  const decoded = jwt.decode(result.data);
   return result.data;
 }
